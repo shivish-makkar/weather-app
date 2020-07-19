c87b21458a5810240a28f0c4139ff6a793b04957
Shivish Makkar <makkar@wisc.edu>
2020-07-19 18:48:29 -0500

    Updates

---
+++ precache-manifest.362514c04843b856a9089cf76dcfa3c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849d2f80679f291848b61495a8f825d6",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "275007923402e288f824",
+    "url": "/weather-app/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "b394a71c6f9f558e71e8",
+    "url": "/weather-app/static/css/main.4f970c73.chunk.css"
+  },
+  {
+    "revision": "275007923402e288f824",
+    "url": "/weather-app/static/js/2.3360a4e7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weather-app/static/js/2.3360a4e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b394a71c6f9f558e71e8",
+    "url": "/weather-app/static/js/main.6ce129f0.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
